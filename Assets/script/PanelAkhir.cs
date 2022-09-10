57a86adc92a0153bf5b2b4db3cf47fd0802be516
Deri Kurniawan <65839958+deku31@users.noreply.github.com>
2022-09-10 17:31:40 +0700

    new feature

--- Assets/script/PanelAkhir.cs
+++ Assets/script/PanelAkhir.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PanelAkhir : MonoBehaviour
 {
     private ManagerSoal Soal;
-    [SerializeField] private Text scoreAkhir;
+    [SerializeField] private string HeaderText;
+    [SerializeField] private Text Header, scoreAkhir;
 
+    int _ScoreAkhir = 0;
     private void Awake()
     {
         Soal = FindObjectOfType<ManagerSoal>();
+        _ScoreAkhir = 0;
     }
     private void Start()
     {
         scoreAkhir.text = Soal.jawabanBenar.ToString();
+        Header.text = HeaderText;
+    }
+
+    public void navigator(string Tujuan)
+    {
+        SceneManager.LoadScene(Tujuan);
     }
 }
